721525b578b0e8cd8edc28e4bb856af62fd5e999
Alexander <alexanderdimitrov333@gmail.com>
2020-03-23 23:39:32 +0200

    Added changes to both Indecision and Counter app.I changed the UI for a bit better user experience and also inclded local storage support for both apps. The count for the counter app is being saved in every time the counter is being changed and also the Indecision App

--- 1-Welcome/IndesicionApp/src/playground/counter-example-component-state.js
+++ 1-Welcome/IndesicionApp/src/playground/counter-example-component-state.js
@@ -1,3 +1,4 @@
+//Challenge - setup default prop value to 0
 class Counter extends React.Component{
     constructor(props){
         super(props);
@@ -5,8 +6,22 @@ class Counter extends React.Component{
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleRestart = this.handleRestart.bind(this);
         this.state = {
-            count:0
+            count: 0
         }
+    }
+    componentDidMount(){
+        console.log("App Started");
+        const json = localStorage.getItem("count");
+        const countJson = JSON.parse(json);
+        console.log(countJson);
+        this.setState(() => ({
+            count: countJson
+          }))
+    }
+    componentDidUpdate(prevState,prevProps){
+        console.log("Updated");
+        const json = JSON.stringify(this.state.count);
+        localStorage.setItem("count",json);
     }
     handleAddOne(){
         this.setState((prevState) => {
@@ -32,18 +47,29 @@ class Counter extends React.Component{
     render(){
         return(
             <div>
-                <h1>Count:{this.state.count}</h1>
-                <button onClick = {this.handleAddOne}>+1</button>
-                <button onClick = {this.handleMinusOne}>-1</button>
-                <button onClick = {this.handleRestart}>reset</button>
+                <h1>Count: <span id="count">{this.state.count}</span></h1>
+                <button id="plusButton" onClick = {this.handleAddOne}>+1</button>
+                <button id="minusButton"onClick = {this.handleMinusOne}>-1</button>
+                <button id="resetButton"onClick = {this.handleRestart}>reset</button>
             </div>
         );
     }
 }
+//Left from the exercise when we used defaultProps
+/*
+Counter.defaultProps = {
+    count: 0
+}; 
+*/
 
 // Create 3 methods : handleAddOne, handleMinusOne, handleReset
 // Use console.log to print the method name
 // Wire up onClick & bind in the constructor 
 
 const counterDiv = $("#counter-div")[0];
+//if we want to start the count from 100
+/*
+ReactDOM.render(<Counter count = {100}/>,counterDiv);
+*/
+//if we do not specify count value 
 ReactDOM.render(<Counter />,counterDiv);
