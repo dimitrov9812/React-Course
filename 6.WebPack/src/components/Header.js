3f67c46ec6ec311073ccb608cbe5907e6c7c2a5d
Alexander <alexanderdimitrov333@gmail.com>
2020-03-29 20:27:48 +0300

    Getting familiar with the writing of scss. New naming convention learned -> the BEM  wich will make it easier to read and will improve the oriantation in the file

--- 6.WebPack/src/components/Header.js
+++ 6.WebPack/src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 
   //Header stateless function
   const Header = (props) => (
-      <div id="head">
-        <h1>{props.title}</h1>
-       {props.subtitle &&  <h2>{props.subtitle}</h2>}
+      <div className="header">
+        <h1 className="header__title">{props.title}</h1>
+       {props.subtitle &&  <h2 className="header__subtitle">{props.subtitle}</h2>}
       </div>
     );
   //Set Default props values for the Header component
