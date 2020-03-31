ad45c54f049a8b68fda24512c4ad24766b65bfca
Alexander <alexanderdimitrov333@gmail.com>
2020-03-31 10:34:32 +0300

    I made a 404 error page which shows on entering a wrong link and also explored the Link and NavLink keyword wich are in the focus of client side routing and with their help we avoid the full page refresh. I made a navbar wich always stays on top and in it we have a link to all of our pages for now.

--- 9.BudgetApp/src/app.js
+++ 9.BudgetApp/src/app.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import { BrowserRouter, Route} from 'react-router-dom';
+//we use the 'Switch' to make the 404 page available
+import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 
 
 const ExpenseDashboardPage = () => (
@@ -34,34 +35,52 @@ const HelpPage = () => (
   </div>
 );
 
-
+const NotFoundPage = () => (
+  <div>
+    404! <Link to="/">Go home</Link>
+  </div>
+);
+const Header = () => (
+  <header>
+    <h1>Budget App</h1>
+  </header>
+);
 // In this jsx we add them and they will be succesfully rendered when we visit the links
 // I added links to test if the link will direct the user through the pages.
 const routes = (
   <BrowserRouter>
-    <div>
-      <a href="http://localhost:8080/">Home</a><br/>
-      <a href="http://localhost:8080/create">Create Expense Page</a><br/>
-      <a href="http://localhost:8080/edit">Edit Expense</a><br/>
-      <a href="http://localhost:8080/help">Help</a><br/>
-      <Route 
-          path='/'
-          component={ExpenseDashboardPage}
-          exact = {true}
-      />
-      <Route 
-          path='/create'
-          component={AddExpensePage}
-      />
-       <Route 
-          path='/edit'
-          component={EditExpensePage}
-      />
-       <Route 
-          path='/help'
-          component={HelpPage}
-      />
-    </div>
+  <div>
+      <Header />
+
+        <NavLink to="/" activeClassName="is-active" exact={true}>ExpenseDashboard</NavLink><br/>
+        <NavLink to="/create" activeClassName="is-active">Add Expense</NavLink><br/>
+        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink><br/>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink><br/>
+
+      <switch>
+        <Route 
+            path='/'
+            component={ExpenseDashboardPage}
+            exact = {true}
+        />
+        <Route 
+            path='/create'
+            component={AddExpensePage}
+        />
+        <Route 
+            path='/edit'
+            component={EditExpensePage}
+        />
+        <Route 
+            path='/help'
+            component={HelpPage}
+        />
+        <Route 
+            component={NotFoundPage}
+        />
+      </switch>
+  </div>
+     
   </BrowserRouter>
 );
   const componentDiv = $("#app")[0];
