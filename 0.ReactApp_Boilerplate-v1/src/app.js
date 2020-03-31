19a1ac5e5c739fb85b9e1df1fbca8feba34a0343
Alexander <alexanderdimitrov333@gmail.com>
2020-03-31 06:54:41 +0300

    I made a copy of the ready indecision app and deleted some parts of it. I left only the base file structure and configure files which we will use in the next application. I ended up with a project ready boiler plate which i will save and include in my repositorty also. I can later use it for a faster start in creating a project.

---
+++ 0.ReactApp_Boilerplate-v1/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+  const componentDiv = $("#app")[0];
+  ReactDOM.render(<p>This is my boilerplate</p>,componentDiv);
